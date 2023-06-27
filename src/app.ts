3c40bd29b6c2252486c898eee213d759773bd266
heitor lourenco <juniorio01@hotmail.com>
2023-06-27 00:16:09 -0300

    chore: config cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import Routers from "./routes";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json())
+
+app.use(cors());
+
+app.use(cors({
+  origin: "*", 
+}))
 
 Routers(app)
 
